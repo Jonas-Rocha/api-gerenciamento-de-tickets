735fb03268ce36cf1c68a363ca1d0203b6efbb9c
Jonas Rocha <jonasrpnstudy@gmail.com>
2026-02-05 12:30:50 -0300

    Criando o objeto de ticket

--- src/controllers/tickets/create.js
+++ src/controllers/tickets/create.js
@@ -2,7 +2,22 @@
 para poder usar eles na ordem que quiser, se tirar das chaves,
 os parâmetros precisariam seguir a mesma sequência. então é bom
 desestruturar a request e a response*/
+import { randomUUID } from "node:crypto"
 
 export function create({ request, response }) {
-    return response.end("Criado com sucesso!")
+    const { equipament, description, user_name } = request.body
+
+    const ticket = {
+        id: randomUUID(),
+        equipament,
+        description,
+        user_name,
+        status: "open",
+        created_at: new Date(),
+        updated_at: new Date()
+    }
+
+
+
+    return response.end(JSON.stringify(ticket))
 }
