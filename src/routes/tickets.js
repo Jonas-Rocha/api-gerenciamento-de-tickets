00f2b74ed24a76e0870a86a1876c212b37066c91
Jonas Rocha <jonasrpnstudy@gmail.com>
2026-02-09 18:09:08 -0300

    Rota para atualizar ticket

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,22 +1,28 @@
 import { create } from "../controllers/tickets/create.js";
 import { index } from "../controllers/tickets/index.js";
+import { update } from "../controllers/tickets/update.js";
 
 //Criando as rotas
 export const tickets = [
-    {
-        method: "POST",
-        path: "/tickets",
-        /*O controller é a função que vai executar alguma coisa quando a rota for chamada*/
-        // controller: (request, response) => {
-        //     response.end("Criado com sucesso!")
-        // }
-        controller: create,
+  {
+    method: "POST",
+    path: "/tickets",
+    /*O controller é a função que vai executar alguma coisa quando a rota for chamada*/
+    // controller: (request, response) => {
+    //     response.end("Criado com sucesso!")
+    // }
+    controller: create,
+  },
 
-    },
+  {
+    method: "GET",
+    path: "/tickets",
+    controller: index,
+  },
 
-    {
-        method: "GET",
-        path: "/tickets",
-        controller: index,
-    }
-]
+  {
+    method: "PUT",
+    path: "/tickets/:id",
+    controller: update,
+  },
+];
